3ff43cf987683acfcfdb45c5d459e0c177395cba
lihao <15502354225@163.com>
2022-01-13 17:58:04 +0800

    实例应用于多页面应用

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Login = (props: Props) => {
+  return <div>首页</div>;
+};
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const NotFound = (props: Props) => {
+  return <h3>未找到页面</h3>;
+};
